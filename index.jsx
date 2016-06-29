afc37922ebce1b9b2f5a0f88dfb569c023d8c04e
Nicolas McCurdy <thenickperson@gmail.com>
2016-06-29 12:42:16 -0400

    Format code for standard

--- index.jsx
+++ index.jsx
@@ -1,12 +1,18 @@
-function Item(props) {
-  return <li>{props.data.name}</li>
+function Item (props) {
+  return (
+    <li>
+      {props.data.name}
+    </li>
+  )
 }
 
-function ItemList(props) {
+function ItemList (props) {
   return (
     <div>
       <h2>{props.type}</h2>
-      <ul>{props.items.map((item) => <Item key={item.id} data={item}/>)}</ul>
+      <ul>
+        {props.items.map((item) => <Item key={item.id} data={item} />)}
+      </ul>
     </div>
   )
 }
@@ -14,25 +20,28 @@ function ItemList(props) {
 class Results extends React.Component {
   constructor () {
     super()
-    
+
     this.state = {
       results: []
     }
   }
-  
+
   componentDidMount () {
     fetch('https://api.spotify.com/v1/search?type=artist,album,track,playlist&q=' + encodeURIComponent(this.props.query))
       .then((response) => response.json())
-      .then((results) => { this.setState({ results }) })
+      .then((results) => {
+        this.setState({ results})})
   }
 
   render () {
     var resultKeys = Object.keys(this.state.results)
-    
-    return <div>
-      {resultKeys.map((type) => <ItemList key={type} type={type} items={this.state.results[type].items}/>)}
-    </div>
+
+    return (
+      <div>
+        {resultKeys.map((type) => <ItemList key={type} type={type} items={this.state.results[type].items} />)}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<Results query="Swans"/>, document.getElementById('results'))
+ReactDOM.render(<Results query='Swans' />, document.getElementById('results'))
